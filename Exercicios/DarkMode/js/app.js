ab4822c379b7dd498aa892ed72c2480672559486
Leonardo Santino <79323700+leonardosantino@users.noreply.github.com>
2021-08-20 13:24:38 -0300

    Exercicio dark mode

---
+++ Exercicios/DarkMode/js/app.js
@@ -0,0 +1,9 @@
+let body = document.querySelector('body');
+let checkBox = document.getElementById("SwitchCheck");
+
+let change = () => {
+    if( checkBox.checked){
+        return body.style.backgroundColor = "#000000";
+    }
+    else { return body.style.backgroundColor = '#dbdbdb'}
+}
