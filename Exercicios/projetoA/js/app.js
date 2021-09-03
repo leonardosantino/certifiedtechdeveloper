5100a3238a01dcbff4187a653bf2a776aa1782ad
Leonardo Santino <79323700+leonardosantino@users.noreply.github.com>
2021-09-03 13:29:31 -0300

    Projeto dogs

--- Exercicios/projetoA/js/app.js
+++ Exercicios/projetoA/js/app.js
@@ -2,17 +2,24 @@
 const container = document.querySelector('.grid-container')
 const urlimg = document.getElementById('urlimg')
 const descricao = document.getElementById('descricao')
-const cadastrar = document.getElementById('cadastrar')
+const form = document.querySelector('form')
+
 // Função para acionar o button e inserir o conteudo
-cadastrar.onclick = () => {
+form.onsubmit = function (event) {
+  event.preventDefault();
+
   // criar div para atribuir o conteudo
   const div = document.createElement('div')
+
   // Inserir dentro dessa div o conteudo dos inputs
   div.innerHTML = `<img class='grid-item grid-item-2 img-fluid' src="${urlimg.value}" alt="">`
   div.innerHTML += `<p>${descricao.value}</p>`
+
   // fazer o append da div dentro do container
   container.appendChild(div)
+
   // resetar valores dos inputs
   urlimg.value = ''
-  descricao.value = '' 
+  descricao.value = ''
+  
 }
