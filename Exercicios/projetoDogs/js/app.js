bde0b381db5c163bb6c54a727eab9c775d9e99a5
Leonardo Santino <79323700+leonardosantino@users.noreply.github.com>
2021-09-03 13:57:43 -0300

    Projeto Dogs

--- Exercicios/projetoDogs/js/app.js
+++ Exercicios/projetoDogs/js/app.js
@@ -4,18 +4,19 @@ const urlimg = document.getElementById('urlimg')
 const descricao = document.getElementById('descricao')
 const form = document.querySelector('form')
 
-// Função para acionar o button e inserir o conteudo
+// Função onsubmit é acionada quando o botão submit e pressionado
 form.onsubmit = function (event) {
+  // previnir a pagina de recarregar e perder os dados quando o botão submit for pressionado
   event.preventDefault();
 
-  // criar div para atribuir o conteudo
+  // criar div para atribuir o conteudo dentro dela
   const div = document.createElement('div')
 
   // Inserir dentro dessa div o conteudo dos inputs
   div.innerHTML = `<img class='grid-item grid-item-2 img-fluid' src="${urlimg.value}" alt="">`
   div.innerHTML += `<p>${descricao.value}</p>`
 
-  // fazer o append da div dentro do container
+  // fazer o append da div dentro do container no body
   container.appendChild(div)
 
   // resetar valores dos inputs
