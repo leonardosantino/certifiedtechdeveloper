d68ce0eba13b315f0939cf8f34b4a0bfb03916f0
Leonardo Santino <79323700+leonardosantino@users.noreply.github.com>
2021-09-03 11:13:16 -0300

    Projeto Dog

---
+++ Exercicios/projetoA/js/app.js
@@ -0,0 +1,18 @@
+// Selecionar elementos para iserção do conteudo
+const container = document.querySelector('.grid-container')
+const urlimg = document.getElementById('urlimg')
+const descricao = document.getElementById('descricao')
+const cadastrar = document.getElementById('cadastrar')
+// Função para acionar o button e inserir o conteudo
+cadastrar.onclick = () => {
+  // criar div para atribuir o conteudo
+  const div = document.createElement('div')
+  // Inserir dentro dessa div o conteudo dos inputs
+  div.innerHTML = `<img class='grid-item grid-item-2 img-fluid' src="${urlimg.value}" alt="">`
+  div.innerHTML += `<p>${descricao.value}</p>`
+  // fazer o append da div dentro do container
+  container.appendChild(div)
+  // resetar valores dos inputs
+  urlimg.value = ''
+  descricao.value = '' 
+}
