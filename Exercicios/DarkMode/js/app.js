415ec01739f4760ee23250c7454d577cbda88e1c
Leonardo Santino <79323700+leonardosantino@users.noreply.github.com>
2021-09-05 10:47:16 -0300

    Delete Exercicios directory

--- Exercicios/DarkMode/js/app.js
+++
@@ -1,7 +0,0 @@
-let body = document.querySelector("body");
-let checkBox = document.getElementById("SwitchCheck");
-
-let change = () => {
-    if (checkBox.checked) {return body.style.backgroundColor = "#000000";}
-    else {return body.style.backgroundColor = "#dbdbdb";}
-}
